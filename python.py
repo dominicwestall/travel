097035caf185be2f8629cb2130d1bcacfc790b01
AndreasLHoward1998 <106036626+AndreasLHoward1998@users.noreply.github.com>
2023-10-19 23:36:00 +1300

    Update python.py
    Started work on the python page of our project!

--- python.py
+++ python.py
@@ -1,1 +1,12 @@
 #code to follow
+from flask import Flask, render_template, request, flash, redirect, url_for
+import sqlite3 as sql
+
+app= Flask(__name__)
+@app.route('/')
+def home():
+  return render_template('homesqlProject.html')
+
+@app.route('/enterdetails')
+def enterdetails():
+  return render_template('enterdetails.html')
