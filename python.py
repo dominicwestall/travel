e6f6bc5dfcba8f6bd4c842df027e68cf2b28d537
dominicwestall <dominicwestall@gmail.com>
2023-10-23 10:04:37 +1300

    Complete browsable build of travel app
    Lots of new Python code and several HTML files, plus images.

--- python.py
+++ python.py
@@ -4,15 +4,40 @@ import sqlite3 as sql
 
 app= Flask(__name__)
 @app.route('/')
-def home():
-  return render_template('homesqlProject.html')
+def index():
+    return render_template('index.html')
 
-@app.route('/enterdetailsProject')
-def enterdetails():
-  return render_template('enterdetails.html')
+@app.route('/searchresults', methods=['POST', 'GET'])
+def search_results():
+    return render_template('searchresults.html')
 
-@app.route('/flightbookedProject')
-def flightbooked():
-  return render_template('flightbooked.html')
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
-@app.route('/aboutProject')
+@app.route('/cart')
+def cart():
+    return render_template('cart.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/reviews')
+def reviews():
+    return render_template('reviews.html')
+
+@app.route('/settings')
+def settings():
+    return render_template('settings.html')
+
+@app.route('/signin')
+def signin():
+    return render_template('signin.html')
+
+@app.route('/registration')
+def registration():
+    return render_template('registration.html')
+
+if __name__=='__main__':
+    app.run(debug=True)
