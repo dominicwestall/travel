de217a4deb04f3daf7522def55cf4bd85d533913
dominicwestall <dominicwestall@gmail.com>
2023-10-26 15:08:08 +1300

    Login and registration functionality
    Python code for login process as well as new user registration. To be used in conjunction with the 'travellogin' db.

--- python.py
+++ python.py
@@ -35,9 +35,52 @@ def settings():
 def signin():
     return render_template('signin.html')
 
+@app.route('/usersignin', methods=['POST','GET'])
+def user_signin():
+    # this is the login functionality
+    # current design of the login system
+    # if authentication is successful, the user is redirected to the home page
+    # if not, the user gets an error message and can try again.
+    if request.method == "POST":
+        msg = None
+        emailaddressofuser = request.form['email']
+        passwordofuser = request.form['password']
+        con = sql.connect("travellogin.db")
+        con.row_factory=sql.Row
+        cur=con.cursor()
+        cur.execute("select * from Users where Email=? and Password=?", (emailaddressofuser, passwordofuser))
+        rows=cur.fetchone()
+        if rows:
+            con.close()
+            return render_template('index.html')
+        else:
+            con.close()
+            msg = "Unsuccessful login. Wrong email address and/or password"
+            return render_template('signin.html', msg=msg)
+
 @app.route('/registration')
 def registration():
     return render_template('registration.html')
+
+@app.route('/registertheuser', methods=['POST','GET'])
+def registertheuser():
+    # code to insert a record into the travel login database.
+    if request.method == 'POST':
+        emailaddressofuser = request.form['email']
+        passwordofuser = request.form['password']
+        if not emailaddressofuser or passwordofuser:
+            with sql.connect("travellogin.db") as con:
+               cur = con.cursor()
+               cur.execute("insert into Users(Email, Password) values (?,?)", (emailaddressofuser, passwordofuser))
+               con.commit()
+               msg = "New user successfully added"
+        else:
+            msg = "New user: error in insert operation"
+            con.rollback()
+            return render_template('registration.html', msg=msg)
+    
+    # send the user back to the home page once successful.
+        return render_template('index.html', msg=msg)
 
 if __name__=='__main__':
     app.run(debug=True)
